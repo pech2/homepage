e11d8d405dee8a019576cb8f816af72bf8b084a1
Peter Chiang <pccchiang@gmail.com>
2026-04-30 14:40:53 -0400

    Update default info

--- src/site.config.ts
+++ src/site.config.ts
@@ -10,7 +10,7 @@ export const siteConfig: SiteConfig = {
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "Astro Cactus",
+	title: "Peter Chiang",
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
 	author: "Peter Chiang",
 	// Used as the default description meta property and webmanifest description
